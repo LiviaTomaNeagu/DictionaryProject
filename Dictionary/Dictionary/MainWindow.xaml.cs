9fea9a4c92948175b26fbfc35302dbfb4439f669
Livia-unitbv <livia.toma@student.unitbv.ro>
2024-03-08 11:11:17 +0200

    Added Administrator-AddWord frontend implementation

--- Dictionary/Dictionary/MainWindow.xaml.cs
+++ Dictionary/Dictionary/MainWindow.xaml.cs
@@ -108,5 +108,46 @@ namespace Dictionary
                 //}
             }
         }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Preia informațiile din TextBox-uri
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+
+            if (username == "1" && password == "2")
+            {
+                txtResult.Text = "Login succes!";
+                txtResult.Visibility = Visibility.Visible;
+                Login.Visibility = Visibility.Hidden;
+                AdminActions.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtResult.Text = "Login failed! Try again.";
+                txtResult.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                var image = new Image();
+                image.Source = new BitmapImage(new Uri(filePath, UriKind.Absolute));
+                ImageButton.Content = image;
+                //de adaugat butonul in stanga si de modificat imaginea
+            }
+
+        }
     }
 }
